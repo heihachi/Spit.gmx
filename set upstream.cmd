8affd6cca3dab83fdd3225a1f56f5d7e6c98313a
Heihachi <Gamehacker953@gmail.com>
2013-11-11 11:48:20 -0500

    [000] Create Repo

---
+++ set upstream.cmd
@@ -0,0 +1,7 @@
+@echo off
+ECHO Please make sure Git is set in the script or in your PATH Env Variable.
+ECHO Use carefully unwanted pushes or pushes that crash the game will be removed.
+pause
+ECHO Setting up Upstream
+git remote add upstream https://github.com/CU-Computer-Science-Club/Concorvania.gmx.git
+pause
