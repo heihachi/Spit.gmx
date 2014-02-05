99ba8ff40406757327382ae152c2bf2e5fd6b486
Heihachi <gamehacker953@gmail.com>
2014-02-05 07:39:07 -0500

    Updated to existing functions

--- push.cmd
+++ push.cmd
@@ -2,8 +2,7 @@
 ECHO Please make sure Git is set in the script or in your PATH Env Variable.
 ECHO Use carefully unwanted pushes or pushes that crash the game will be removed.
 pause
-ECHO Setting up Upstream
-git remote add upstream https://github.com/CU-Computer-Science-Club/Spit.gmx.git
+ECHO Only for pushing to Concords CSC repo
 set /p user=Enter your username: 
-git branch %user%
+git push upstream %user%
 pause
